69624a3e76e6594e84d371d211a1559c1e3a92e9
mingyeongmo <mingyeongmp@naver.com>
2021-07-05 23:34:55 +0900

    useState 간단 예제

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,27 +1,20 @@
-import React from 'react';
-
-function showName() {
-    console.log("Mike");
-}
-function showAge(age) {
-    console.log(age);
-}
-function showText(txt) {
-    console.log(txt);
-}
+import React, {useState} from 'react';
 
 const Hello = () => {
+
+    //let name = "Mike";
+    const [name, setName] = useState('Mike');
+
+    function changeName() {
+        const newName = name === 'Mike' ? 'Jane' : 'Mike';
+        setName(newName);
+    }
+
     return (
         <div>
-            <button onClick={showName}>Show name</button>
-            <button onClick={() => {
-                showAge(30);
-            }}>Show age</button>
-            <input type="text" onChange={e => {
-                const txt = e.target.value;
-                showText(txt);
-            }} />
-            {/* target= input태그 value는 작성한 값 */}
+            <h1>state</h1>
+            <h2>{name}</h2>
+            <button onClick={changeName}>Change</button>
         </div>
     );
 };
