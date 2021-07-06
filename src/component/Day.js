706c190753cdb9e671cb94266c15df90aad05976
mingyeongmo <mingyeongmp@naver.com>
2021-07-06 23:51:26 +0900

    useEffect, fetch()

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import dummy from '../db/data.json';
+import React, { useState,useEffect } from 'react';
+
 import Word from './Word';
-import { useParams } from "react-router-dom";
+import { useParams} from "react-router-dom";
 
 // dummy.words
 
@@ -11,23 +11,28 @@ const Day = () => {
     //const day = useParams().day;
     //const a = useParams();
     //const day = a.day
-    const wordList = dummy.words.filter(word => word.day === Number(day) )
+   // const wordList = dummy.words.filter(word => word.day === Number(day) )
+    const [words, setWords] = useState([]);
 
-    
-  
-    
+    useEffect(() => {
+        fetch(`http://localhost:3007/words?day=${day}`)
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setWords(data);
+        })
+    }, [day]);
 
     return (
         <>
         <h2>Day {day}</h2>
         <table>
             <tbody>
-            {wordList.map(word => (
+            {words.map(word => (
                 <Word word={word} key={word.id} />
             ))}
             </tbody>
-            
-            
         </table>
         </>
     );
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,14 +1,26 @@
-import dummy from "../db/data.json";
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 
 
 const DayList = () => {
-    console.log(dummy);
+    const [days, setDays] = useState([]);
+    
+    
+    useEffect(() => {
+        fetch("http://localhost:3007/days")
+        .then(res => { // response
+            return res.json()
+        })
+        .then(data => {
+            setDays(data);
+        });
+    }, []); // 의존성 배열
+
     return (
         <>
             <ul className="list_day">
-                {dummy.days.map(day => (
+                {days.map(day => (
                     <li key={day.id}>
                         <Link to={`/day/${day.day}`}>
                         Day {day.day}
